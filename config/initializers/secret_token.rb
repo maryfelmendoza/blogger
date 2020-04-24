658a87a277c196aad7263eb64ea60c876843c7ef
maryfelmendoza <maryfelmendoza@hotmail.com>
2020-04-24 16:48:25 +0000

    Initialize repository and Setting Up Router

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Blogger::Application.config.secret_key_base = '5f3488396a6d2783f402334b7c598ae0673a507a026df442d0814c68fa22e4db5a45002e0dd0fd38ce3368153c6c271746226e766d01b4cc65283d1202d4dd15'
